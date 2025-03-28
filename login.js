d10d597d98432d82239ef18e4e3ed8d563e6f166
Thomas <98847567+Thomas980130@users.noreply.github.com>
2025-03-29 03:46:17 +0800

    Update login.js

--- login.js
+++ login.js
@@ -12,16 +12,21 @@ document.getElementById('loginForm').addEventListener('submit', async (event) =>
     };
 
     try {
-        const response = await fetch('https://script.google.com/macros/s/AKfycbw9Lu74oo49MUZl48p7ptkHb5l_ZODiGu_-JQWaDMJ_S-gbbb-ede0aOhM2RSvj8p-S/exec', {
+        const response = await fetch('https://cors-anywhere.herokuapp.com/https://script.google.com/macros/s/AKfycbw9Lu74oo49MUZl48p7ptkHb5l_ZODiGu_-JQWaDMJ_S-gbbb-ede0aOhM2RSvj8p-S/exec', {
             method: 'POST',
-            mode: 'no-cors', // 使用 no-cors 模式
             headers: {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify(data)
         });
 
-        alert('登入成功！');
+        const result = await response.json();
+        if (result.success) {
+            alert('登入成功！');
+            console.log(result);
+        } else {
+            alert(result.message);
+        }
     } catch (error) {
         console.error('Error:', error);
         alert('發生錯誤，請稍後再試！');
